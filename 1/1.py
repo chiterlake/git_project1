420526dd5549a0b6cda501d174abb68195d9ef4b
chiterlake <sir.lomakin13@yandex.ru>
2020-12-07 11:34:58 +0300

    last comm

--- 1/1.py
+++ 1/1.py
@@ -1,7 +1,11 @@
 def main():
     print('My first git program')
     print('And I change it every day')
-    # https://github.com/chiterlake/git_project1
+    print('Again')
+    print('UFO came and added this line')
+
+
+# https://github.com/chiterlake/git_project1
 
 if __name__ == '__main__':
     main()
